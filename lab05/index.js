10018bce991d5eb203cc66753cd5a08131a4877a
Ingita <imoiseienko@knights.ucf.edu>
2020-02-14 14:54:32 -0500

    lab5
    lab5

---
+++ lab05/index.js
@@ -0,0 +1,42 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+const pokemon = require('json-pokemon');
+const chalk = require("chalk");
+
+
+App.use("/", Express.static("public"));
+
+
+
+App.get("/id/:id", (req, res) => {
+    let result = {"error": "invalid"};
+    pokemon.forEach((value)=> {
+        if(value.id == req.params.id) {
+             result = value;
+        }
+    });
+   if(result.error){ 
+    console.log(chalk.red(req.path));
+   }
+     else {console.log(chalk.green(req.path));}
+     res.send(result);
+});
+
+App.get("/name/:name", (req, res) => {
+    let result = {"error": "invalid"};
+    pokemon.forEach((value)=> {
+        if(value.name == req.params.name) {
+            result = value;
+        }
+    });
+    if(result.error){ 
+        console.log(chalk.red(req.path));
+       }
+         else {console.log(chalk.green(req.path));}
+         res.send(result);
+});
+
+App.listen(port, () => {
+    console.log("Server is running!");
+});
